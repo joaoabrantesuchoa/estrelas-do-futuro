87b9cb8088c88a30d0c238083e39d675e3f1c157
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-11-24 17:34:01 -0300

    chore: install vitest and create new script to run tests

--- server/app.js
+++ server/app.js
@@ -1,19 +1,17 @@
 import express from "express";
-import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
 import studentRouter from "./routes/student.js";
 
 const app = express();
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
-
-app.get("/", (response) => {
-  return response.status(200).send("OK");
+app.get("/", (req, res) => {
+  return res.status(200).send("OK");
 });
 
 app.use("/students", studentRouter);
 
 connectDB();
+
+export default app;
+
