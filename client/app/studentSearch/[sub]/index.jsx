862b44dc5edaa178fed1d172124d595dc5279721
Laysa <laysa.silva@ccc.ufcg.edu.br>
2023-12-23 22:35:03 -0300

    feat: starting the student list page

--- client/app/studentSearch/[sub]/index.jsx
+++ client/app/studentSearch/[sub]/index.jsx
@@ -1,6 +1,23 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import List from "../components/List/index";
+
+const alunos = [{ name: "Carol", name: "Jubileu" }];
 
 function StudentList() {
-
+  return (
+    <View style={styles.container}>
+      <View>
+        {alunos.map((index, aluno) => {
+          return <List key={index} name={aluno.name} />;
+        })}
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text>Adicionar aluno</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 export default StudentList;
---
+++ client/app/studentSearch/[sub]/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  button: {
+    width: 229,
+    height: 42,
+    backgroundColor: "#DDDDDD",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 15,
+    borderColor: "#000",
+    borderWidth: 1,
+    shadowColor: "#000",
+  },
+});
