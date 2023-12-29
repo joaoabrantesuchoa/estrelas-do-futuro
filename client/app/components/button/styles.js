1053b110db83ffe2deeefa9f6b317216b41aa295
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-12-29 11:10:30 -0300

    feat(client): add reusable button component

---
+++ client/app/components/button/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  button: {
+    width: 229,
+    height: 42,
+    backgroundColor: "#DDDDDD",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 15,
+    borderColor: "#000",
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    marginBottom: 8,
+  },
+});
