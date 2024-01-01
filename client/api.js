2ac90d826f4873348381016dd85d9ba5d9a42b72
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2024-01-01 11:54:10 -0300

    refactor(client): add ip to make requests from android

--- client/api.js
+++ client/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-export async function fetchStudents() {
+export async function fetchStudents(studentSub) {
   try {
-    const response = await axios.get("http://localhost:5555/students");
+    let url = "http://192.168.1.3:5555/students";
+    if (studentSub !== "all") {
+      url += `?category=${studentSub}`;
+    }
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
     console.error("Erro ao buscar estudantes:", error);
