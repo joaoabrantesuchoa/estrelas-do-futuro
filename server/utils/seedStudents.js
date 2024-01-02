c97b04cac2fd996de218c8f45276bf3b1bdf103a
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2024-01-02 11:47:22 -0300

    feat(server): add seed for create custom students

--- server/utils/seedStudents.js
+++ server/utils/seedStudents.js
@@ -2,7 +2,7 @@ import { connectDB } from "../db.js";
 import Student from "../models/student.js";
 
 async function seedStudents() {
-  await connectDB()
+  await connectDB();
 
   await Student.deleteMany();
 
@@ -38,6 +38,83 @@ async function seedStudents() {
       responsablePhone: "11987654323",
       medicalObservations: "Diabetes tipo 1",
       position: "Meio-campo",
+      photoUrl: "url_da_foto",
+    },
+    {
+      name: "Lucas Lima",
+      birthDate: "04/04/2013",
+      motherName: "Lúcia Lima",
+      fatherName: "Luís Lima",
+      phone: "11987654324",
+      responsablePhone: "11987654324",
+      medicalObservations: "Nenhuma",
+      position: "Goleiro",
+      photoUrl: "url_da_foto",
+    },
+    {
+      name: "Gabriela Gomes",
+      birthDate: "05/05/2014",
+      motherName: "Gisele Gomes",
+      fatherName: "Gilberto Gomes",
+      phone: "11987654325",
+      responsablePhone: "11987654325",
+      medicalObservations: "Asma",
+      position: "Defensora",
+      photoUrl: "url_da_foto",
+    },
+    {
+      name: "Rafael Ribeiro",
+      birthDate: "06/06/2015",
+      motherName: "Renata Ribeiro",
+      fatherName: "Ricardo Ribeiro",
+      phone: "11987654326",
+      responsablePhone: "11987654326",
+      medicalObservations: "Nenhuma",
+      position: "Atacante",
+      photoUrl: "url_da_foto",
+    },
+    {
+      name: "Fernanda Ferreira",
+      birthDate: "07/07/2016",
+      motherName: "Fátima Ferreira",
+      fatherName: "Fábio Ferreira",
+      phone: "11987654327",
+      responsablePhone: "11987654327",
+      medicalObservations: "Alergia a lactose",
+      position: "Meio-campo",
+      photoUrl: "url_da_foto",
+    },
+    {
+      name: "Eduardo Esteves",
+      birthDate: "08/08/2017",
+      motherName: "Elaine Esteves",
+      fatherName: "Edson Esteves",
+      phone: "11987654328",
+      responsablePhone: "11987654328",
+      medicalObservations: "Nenhuma",
+      position: "Defensor",
+      photoUrl: "url_da_foto",
+    },
+    {
+      name: "Isabela Ibarra",
+      birthDate: "09/09/2018",
+      motherName: "Irene Ibarra",
+      fatherName: "Igor Ibarra",
+      phone: "11987654329",
+      responsablePhone: "11987654329",
+      medicalObservations: "Nenhuma",
+      position: "Atacante",
+      photoUrl: "url_da_foto",
+    },
+    {
+      name: "Daniel Dias",
+      birthDate: "10/10/2019",
+      motherName: "Daniela Dias",
+      fatherName: "Danilo Dias",
+      phone: "11987654330",
+      responsablePhone: "11987654330",
+      medicalObservations: "Nenhuma",
+      position: "Goleiro",
       photoUrl: "url_da_foto",
     },
   ];
