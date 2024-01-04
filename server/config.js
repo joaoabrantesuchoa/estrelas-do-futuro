e3eedb80e6d60afe7c6f3195d4b83aed7c8f1150
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2024-01-04 13:42:34 -0300

    chore(server): adjust project structure and add back end environment variables

--- server/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://joao-abrantes-uchoa:8R8Re9jvpoKQvbM8@estrelas-do-furuto.hnqfzl9.mongodb.net/?retryWrites=true&w=majority";
--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-import { mongoDBURL } from "./config.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(mongoDBURL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("App is connected to the database");
   } catch (error) {
     console.error("Error connecting to the database:", error.message);
