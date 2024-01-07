4e599aadb72b7d384de14f0fcaa43a4ccf2330d1
Laysa <laysa.silva@ccc.ufcg.edu.br>
2024-01-07 16:36:46 -0300

    feat: add placeHolder to empty fields

--- client/app/student/create/index.js
+++ client/app/student/create/index.js
@@ -37,31 +37,37 @@ function Registration() {
       <TextBox
         name={"Nome do aluno"}
         onChangeText={setStudentName}
+        placeHolder={"Nome do aluno"}
         value={studentName}
       />
       <TextBox
         name={"Data de nascimento"}
         onChangeText={setBirthDate}
+        placeHolder={"DD/MM/AAAA"}
         value={birthDate}
       />
       <TextBox
         name={"Nome do pai"}
         onChangeText={setFatherName}
+        placeHolder={"Nome do pai"}
         value={fatherName}
       />
       <TextBox
         name={"Nome da mãe"}
         onChangeText={setMotherName}
+        placeHolder={"Nome da mãe"}
         value={motherName}
       />
       <TextBox
         name={"Telefone do responsável"}
         onChangeText={setGuardianPhone}
+        placeHolder={"DD9XXXXXXXX"}
         value={guardianPhone}
       />
       <TextBox
         name={"Observações médicas"}
         onChangeText={setMedicalNotes}
+        placeHolder={"Observações médicas"}
         value={medicalNotes}
       />
       <TouchableOpacity
