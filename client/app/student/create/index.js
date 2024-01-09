c5ff8b2b49884b628bbc921343b2c91dd8267e14
Laysa <laysa.silva@ccc.ufcg.edu.br>
2024-01-09 16:24:40 -0300

    feat: add validation to received data

--- client/app/student/create/index.js
+++ client/app/student/create/index.js
@@ -6,6 +6,8 @@ import { styles } from "./styles";
 import { addStudent } from "../../../api";
 import { useRouter } from "expo-router";
 import BackArrow from "../../components/backArrow";
+import * as Yup from "yup";
+import { Alert } from "react-native";
 
 function Registration() {
   const router = useRouter();
@@ -16,6 +18,64 @@ function Registration() {
   const [motherName, setMotherName] = useState("");
   const [guardianPhone, setGuardianPhone] = useState("");
   const [medicalNotes, setMedicalNotes] = useState("");
+  const [studentNameError, setStudentNameError] = useState("");
+  const [birthDateError, setBirthDateError] = useState("");
+  const [fatherNameError, setFatherNameError] = useState("");
+  const [motherNameError, setMotherNameError] = useState("");
+  const [guardianPhoneError, setGuardianPhoneError] = useState("");
+  const [medicalNotesError, setMedicalNotesError] = useState("");
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("O nome do aluno é obrigatório."),
+    birthDate: Yup.string()
+      .required("A data de nascimento é obrigatória.")
+      .matches(
+        /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/,
+        "A data deve estar no formato DD/MM/AAAA."
+      ),
+    fatherName: Yup.string().required("O nome do pai é obrigatório."),
+    motherName: Yup.string().required("O nome da mãe é obrigatório."),
+    responsablePhone: Yup.string()
+      .matches(
+        /^(?:\d{2})?\d{9}$/,
+        "O telefone do responsável deve estar no formato DD9XXXXXXXX."
+      )
+      .length(11, "O número de telefone deve ter 11 dígitos, incluindo o DDD.")
+      .required("O telefone do responsável é obrigatório."),
+    medicalObservations: Yup.string().required(
+      "As observações médicas são obrigatórias."
+    ),
+  });
+
+  const handleStudentNameChange = (text) => {
+    setStudentName(text);
+    setStudentNameError("");
+  };
+
+  const handleBirthDateChange = (text) => {
+    setBirthDate(text);
+    setBirthDateError("");
+  };
+
+  const handleFatherNameChange = (text) => {
+    setFatherName(text);
+    setFatherNameError("");
+  };
+
+  const handleMotherNameChange = (text) => {
+    setMotherName(text);
+    setMotherNameError("");
+  };
+
+  const handleGuardianPhoneChange = (text) => {
+    setGuardianPhone(text);
+    setGuardianPhoneError("");
+  };
+
+  const handleMedicalNotesChange = (text) => {
+    setMedicalNotes(text);
+    setMedicalNotesError("");
+  };
 
   const registerStudent = useCallback(async () => {
     const studentData = {
@@ -27,8 +87,45 @@ function Registration() {
       medicalObservations: medicalNotes,
     };
 
-    await addStudent(studentData);
-    router.back();
+    try {
+      await validationSchema.validateSync(studentData, { abortEarly: false });
+      addStudent(studentData);
+      function showSuccessPopup() {
+        Alert.alert(
+          "Operação bem sucedida",
+          "Aluno cadastrado com sucesso!",
+          [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+          { cancelable: false }
+        );
+      }
+      showSuccessPopup();
+      router.back();
+    } catch (error) {
+      error.inner.forEach((err) => {
+        switch (err.path) {
+          case "studentName":
+            setStudentNameError(err.message);
+            break;
+          case "birthDate":
+            setBirthDateError(err.message);
+            break;
+          case "fatherName":
+            setFatherNameError(err.message);
+            break;
+          case "motherName":
+            setMotherNameError(err.message);
+            break;
+          case "guardianPhone":
+            setGuardianPhoneError(err.message);
+            break;
+          case "medicalNotes":
+            setMedicalNotesError(err.message);
+            break;
+          default:
+            break;
+        }
+      });
+    }
   });
 
   return (
@@ -36,39 +133,45 @@ function Registration() {
       <BackArrow />
       <TextBox
         name={"Nome do aluno"}
-        onChangeText={setStudentName}
+        onChangeText={handleStudentNameChange}
         placeHolder={"Nome do aluno"}
         value={studentName}
+        errorMessage={studentNameError}
       />
       <TextBox
         name={"Data de nascimento"}
-        onChangeText={setBirthDate}
+        onChangeText={handleBirthDateChange}
         placeHolder={"DD/MM/AAAA"}
         value={birthDate}
+        errorMessage={birthDateError}
       />
       <TextBox
         name={"Nome do pai"}
-        onChangeText={setFatherName}
+        onChangeText={handleFatherNameChange}
         placeHolder={"Nome do pai"}
         value={fatherName}
+        errorMessage={fatherNameError}
       />
       <TextBox
         name={"Nome da mãe"}
-        onChangeText={setMotherName}
+        onChangeText={handleMotherNameChange}
         placeHolder={"Nome da mãe"}
         value={motherName}
+        errorMessage={motherNameError}
       />
       <TextBox
         name={"Telefone do responsável"}
-        onChangeText={setGuardianPhone}
+        onChangeText={handleGuardianPhoneChange}
         placeHolder={"DD9XXXXXXXX"}
         value={guardianPhone}
+        errorMessage={guardianPhoneError}
       />
       <TextBox
         name={"Observações médicas"}
-        onChangeText={setMedicalNotes}
+        onChangeText={handleMedicalNotesChange}
         placeHolder={"Observações médicas"}
         value={medicalNotes}
+        errorMessage={medicalNotesError}
       />
       <TouchableOpacity
         style={styles.buttonContainer}
--- client/app/student/edit/index.js
+++ client/app/student/edit/index.js
@@ -6,6 +6,8 @@ import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState, useCallback } from "react";
 import { getStudentById, editStudent } from "../../../api";
 import { styles } from "./styles";
+import * as Yup from "yup";
+import { Alert } from "react-native";
 
 function edit() {
   const { studentId } = useLocalSearchParams();
@@ -22,13 +24,19 @@ function edit() {
 
   const router = useRouter();
 
-  const [studentName, setStudentName] = useState("");
-  const [birthDate, setBirthDate] = useState("");
-  const [fatherName, setFatherName] = useState("");
-  const [motherName, setMotherName] = useState("");
-  const [guardianPhone, setGuardianPhone] = useState("");
-  const [position, setPosition] = useState("");
-  const [medicalNotes, setMedicalNotes] = useState("");
+  const [studentName, setStudentName] = useState();
+  const [birthDate, setBirthDate] = useState();
+  const [fatherName, setFatherName] = useState();
+  const [motherName, setMotherName] = useState();
+  const [guardianPhone, setGuardianPhone] = useState();
+  const [position, setPosition] = useState();
+  const [medicalNotes, setMedicalNotes] = useState();
+  const [studentNameError, setStudentNameError] = useState();
+  const [birthDateError, setBirthDateError] = useState();
+  const [fatherNameError, setFatherNameError] = useState();
+  const [motherNameError, setMotherNameError] = useState();
+  const [guardianPhoneError, setGuardianPhoneError] = useState();
+  const [medicalNotesError, setMedicalNotesError] = useState();
 
   useEffect(() => {
     if (studentData.name) {
@@ -54,6 +62,58 @@ function edit() {
     }
   }, [studentData]);
 
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("O nome do aluno é obrigatório."),
+    birthDate: Yup.string()
+      .required("A data de nascimento é obrigatória.")
+      .matches(
+        /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/,
+        "A data deve estar no formato DD/MM/AAAA."
+      ),
+    fatherName: Yup.string().required("O nome do pai é obrigatório."),
+    motherName: Yup.string().required("O nome da mãe é obrigatório."),
+    responsablePhone: Yup.string()
+      .matches(
+        /^(?:\d{2})?\d{9}$/,
+        "O telefone do responsável deve estar no formato DD9XXXXXXXX."
+      )
+      .length(11, "O número de telefone deve ter 11 dígitos, incluindo o DDD.")
+      .required("O telefone do responsável é obrigatório."),
+    medicalObservations: Yup.string().required(
+      "As observações médicas são obrigatórias."
+    ),
+  });
+
+  const handleStudentNameChange = (text) => {
+    setStudentName(text);
+    setStudentNameError("");
+  };
+
+  const handleBirthDateChange = (text) => {
+    setBirthDate(text);
+    setBirthDateError("");
+  };
+
+  const handleFatherNameChange = (text) => {
+    setFatherName(text);
+    setFatherNameError("");
+  };
+
+  const handleMotherNameChange = (text) => {
+    setMotherName(text);
+    setMotherNameError("");
+  };
+
+  const handleGuardianPhoneChange = (text) => {
+    setGuardianPhone(text);
+    setGuardianPhoneError("");
+  };
+
+  const handleMedicalNotesChange = (text) => {
+    setMedicalNotes(text);
+    setMedicalNotesError("");
+  };
+
   const editDataStudent = useCallback(async () => {
     const studentData = {
       name: studentName,
@@ -65,8 +125,45 @@ function edit() {
       medicalObservations: medicalNotes,
     };
 
-    await editStudent(studentId, studentData);
-    router.back();
+    try {
+      await validationSchema.validateSync(studentData, { abortEarly: false });
+      await editStudent(studentId, studentData);
+      function showSuccessPopup() {
+        Alert.alert(
+          "Operação bem sucedida", // Título do pop-up
+          "Sua operação foi concluída com sucesso!", // Mensagem do pop-up
+          [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+          { cancelable: false }
+        );
+      }
+      showSuccessPopup();
+      router.back();
+    } catch (error) {
+      error.inner.forEach((err) => {
+        switch (err.path) {
+          case "studentName":
+            setStudentNameError(err.message);
+            break;
+          case "birthDate":
+            setBirthDateError(err.message);
+            break;
+          case "fatherName":
+            setFatherNameError(err.message);
+            break;
+          case "motherName":
+            setMotherNameError(err.message);
+            break;
+          case "guardianPhone":
+            setGuardianPhoneError(err.message);
+            break;
+          case "medicalNotes":
+            setMedicalNotesError(err.message);
+            break;
+          default:
+            break;
+        }
+      });
+    }
   });
 
   return (
@@ -75,33 +172,43 @@ function edit() {
         <BackArrow />
         <TextBox
           name={"Nome do aluno"}
-          onChangeText={setStudentName}
+          onChangeText={handleStudentNameChange}
           placeHolder={"Nome do aluno"}
           value={studentName}
+          errorMessage={studentNameError}
+          defaultValue={""}
         />
         <TextBox
           name={"Data de nascimento"}
-          onChangeText={setBirthDate}
+          onChangeText={handleBirthDateChange}
           placeHolder={"DD/MM/AAAA"}
           value={birthDate}
+          errorMessage={birthDateError}
+          defaultValue={""}
         />
         <TextBox
           name={"Nome do pai"}
-          onChangeText={setFatherName}
+          onChangeText={handleFatherNameChange}
           placeHolder={"Nome do pai"}
           value={fatherName}
+          errorMessage={fatherNameError}
+          defaultValue={""}
         />
         <TextBox
           name={"Nome da mãe"}
-          onChangeText={setMotherName}
+          onChangeText={handleMotherNameChange}
           placeHolder={"Nome da mãe"}
           value={motherName}
+          errorMessage={motherNameError}
+          defaultValue={""}
         />
         <TextBox
           name={"Telefone do responsável"}
-          onChangeText={setGuardianPhone}
+          onChangeText={handleGuardianPhoneChange}
           placeHolder={"DD9XXXXXXXX"}
           value={guardianPhone}
+          errorMessage={guardianPhoneError}
+          defaultValue={""}
         />
         <TextBox
           name={"Posição do aluno"}
@@ -111,9 +218,11 @@ function edit() {
         />
         <TextBox
           name={"Observações médicas"}
-          onChangeText={setMedicalNotes}
+          onChangeText={handleMedicalNotesChange}
           placeHolder={"Observações médicas"}
           value={medicalNotes}
+          errorMessage={medicalNotesError}
+          defaultValue={""}
         />
         <TouchableOpacity
           style={styles.buttonContainer}
