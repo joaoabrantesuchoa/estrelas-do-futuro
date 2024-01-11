8d4f72948a8345777cafdda05560c047e4f75fbd
Laysa <laysa.silva@ccc.ufcg.edu.br>
2024-01-10 21:45:24 -0300

    feat: add component ButtonAdd

---
+++ client/app/student/evaluation/components/buttonAdd/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    display: "flex",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "243px",
+    height: "217px",
+    flexShrink: 0,
+    borderRadius: "42px",
+    backgroundColor: "#DDDDDD",
+    marginBottom: 100,
+  },
+  imageContainer: {
+    alignItems: "center",
+  },
+});
