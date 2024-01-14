5d320bde9dbc051a732e024aa607405860d729eb
Laysa <laysa.silva@ccc.ufcg.edu.br>
2024-01-14 19:33:05 -0300

    feat: add component button

---
+++ client/app/student/payment/components/button/index.js
@@ -0,0 +1,23 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { textStyles } from "../../../../../styles/fonts";
+import { styles } from "./styles";
+import { useRouter } from "expo-router";
+
+function Button({ name, navigation }) {
+  const router = useRouter();
+
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          router.push(`${navigation}`);
+        }}
+        style={styles.button}
+      >
+        <Text styles={textStyles.buttonText}>{name}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default Button;
